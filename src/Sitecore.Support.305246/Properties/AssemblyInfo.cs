4bba10cf9775cb9bd172120a2a66cd56715ee2ee
Vlad Molibozhenko <vlm@sitecore.net>
2019-05-28 12:50:44 +0800

    Initial LinqToCloudIndex code

---
+++ src/Sitecore.Support.305246/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.305246")]
+[assembly: AssemblyProduct("Sitecore.Support.305246")]
+[assembly: ComVisible(false)]
